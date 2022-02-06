15312d01cf8c9a2c4985319d3251a451be88ec99
kamenrider72 <tiendangtran693@gmail.com>
2022-02-06 10:55:19 +0700

    giao dien ban dau form Play

---
+++ form/form/Form/fPlay.Designer.cs
@@ -0,0 +1,195 @@
+﻿
+namespace Pikachu
+{
+    partial class fPlay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlBoard = new System.Windows.Forms.Panel();
+            this.pnlButton = new System.Windows.Forms.Panel();
+            this.pnlTime = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblHienThiBan = new System.Windows.Forms.Label();
+            this.lblHienThiDiem = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.lblHienThiLuotDoi = new System.Windows.Forms.Label();
+            this.pnlButton.SuspendLayout();
+            this.pnlTime.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlBoard
+            // 
+            this.pnlBoard.Location = new System.Drawing.Point(97, 86);
+            this.pnlBoard.Name = "pnlBoard";
+            this.pnlBoard.Size = new System.Drawing.Size(1134, 666);
+            this.pnlBoard.TabIndex = 0;
+            // 
+            // pnlButton
+            // 
+            this.pnlButton.Controls.Add(this.button5);
+            this.pnlButton.Controls.Add(this.button4);
+            this.pnlButton.Controls.Add(this.button3);
+            this.pnlButton.Controls.Add(this.button2);
+            this.pnlButton.Controls.Add(this.button1);
+            this.pnlButton.Location = new System.Drawing.Point(1237, 86);
+            this.pnlButton.Name = "pnlButton";
+            this.pnlButton.Size = new System.Drawing.Size(195, 666);
+            this.pnlButton.TabIndex = 2;
+            // 
+            // pnlTime
+            // 
+            this.pnlTime.Controls.Add(this.pictureBox1);
+            this.pnlTime.Location = new System.Drawing.Point(12, 86);
+            this.pnlTime.Name = "pnlTime";
+            this.pnlTime.Size = new System.Drawing.Size(70, 666);
+            this.pnlTime.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(16, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(37, 666);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblHienThiBan
+            // 
+            this.lblHienThiBan.AutoSize = true;
+            this.lblHienThiBan.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblHienThiBan.Location = new System.Drawing.Point(187, 33);
+            this.lblHienThiBan.Name = "lblHienThiBan";
+            this.lblHienThiBan.Size = new System.Drawing.Size(44, 28);
+            this.lblHienThiBan.TabIndex = 4;
+            this.lblHienThiBan.Text = "Bàn";
+            // 
+            // lblHienThiDiem
+            // 
+            this.lblHienThiDiem.AutoSize = true;
+            this.lblHienThiDiem.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblHienThiDiem.Location = new System.Drawing.Point(824, 33);
+            this.lblHienThiDiem.Name = "lblHienThiDiem";
+            this.lblHienThiDiem.Size = new System.Drawing.Size(58, 28);
+            this.lblHienThiDiem.TabIndex = 6;
+            this.lblHienThiDiem.Text = "Điểm";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(91, 120);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(68, 177);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(68, 258);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(90, 329);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // lblHienThiLuotDoi
+            // 
+            this.lblHienThiLuotDoi.AutoSize = true;
+            this.lblHienThiLuotDoi.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblHienThiLuotDoi.Location = new System.Drawing.Point(448, 33);
+            this.lblHienThiLuotDoi.Name = "lblHienThiLuotDoi";
+            this.lblHienThiLuotDoi.Size = new System.Drawing.Size(86, 28);
+            this.lblHienThiLuotDoi.TabIndex = 7;
+            this.lblHienThiLuotDoi.Text = "Lượt đổi";
+            // 
+            // fPlay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1444, 803);
+            this.Controls.Add(this.lblHienThiLuotDoi);
+            this.Controls.Add(this.lblHienThiDiem);
+            this.Controls.Add(this.lblHienThiBan);
+            this.Controls.Add(this.pnlTime);
+            this.Controls.Add(this.pnlButton);
+            this.Controls.Add(this.pnlBoard);
+            this.Name = "fPlay";
+            this.Text = "fPlay";
+            this.pnlButton.ResumeLayout(false);
+            this.pnlTime.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlBoard;
+        private System.Windows.Forms.Panel pnlButton;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel pnlTime;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblHienThiBan;
+        private System.Windows.Forms.Label lblHienThiDiem;
+        private System.Windows.Forms.Label lblHienThiLuotDoi;
+    }
+}
